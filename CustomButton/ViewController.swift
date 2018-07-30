285d74ce6aec10c175d8dcc563497a3e144f2f00
Svetlana <svetlana@gmail.com>
2018-07-30 21:58:24 +0300

    Initial Commit

---
+++ CustomButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomButton
+//
+//  Created by Svetlana T on 30.07.2018.
+//  Copyright © 2018 Nemis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
